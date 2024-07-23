2bcaabb53099182d91dbaf5415b83e3520ad6f0a
Luana <luannarpaulaggmail.com>
2024-07-23 13:30:41 -0300

    parte teste

--- frontEnd/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function Home() {
-  return (
-    <div className="bg-byfenix"></div>
-  );
-}
--- frontEnd/tailwind.config.ts
+++ frontEnd/tailwind.config.ts
@@ -2,17 +2,19 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx,png}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx,png}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx,png}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "byfenix": "url('/Assets/byfenix.png')",
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
-  plugins: [],
+  plugins: [require('daisyui')],
 };
 export default config;
